cbc7d1a123e7e30bbc610e9629956827252238e3
lsw <454412569@qq.om>
2022-02-28 20:53:39 +0800

    消息订阅传递数据

---
+++ react脚手架/react_staging/消息订阅传递数据/components/Search/Search.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import React, { Component } from 'react'
+import Pubsub from 'pubsub-js'
+import './Search'
+
+export default class Search extends Component {
+  searchUser = () => {
+    // 连续解构赋值
+    const { keyWordElement: { value } } = this
+    // 发送请求前更新状态 
+    // 发布消息
+    Pubsub.publish('updateState', { isFirst: false, isLoading: true })
+    // 发起网络请求
+    axios.get(`http://localhost:3000/api1/search/users2?q=${value}`).then((res) => {
+      // 发送请求成功后通知List更新状态 
+      // 发布消息
+      Pubsub.publish('updateState', { isLoading: false, data: res.data.items })
+    }).catch(error => {
+      // 发送请求失败后更新状态 
+      Pubsub.publish('updateState', { isLoading: false, err: error.message })
+    })
+  }
+
+  render() {
+    return (
+      <section className="jumbotron">
+        <h3 className="jumbotron-heading">Search Github Users</h3>
+        <div>
+          <input ref={c => this.keyWordElement = c} type="text" placeholder="enter the name you search" />&nbsp;
+          <button onClick={this.searchUser}>Search</button>
+        </div>
+      </section>
+    )
+  }
+}
